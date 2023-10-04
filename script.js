b3db657844ef042ed670202e6bde982f65f81dab
azzamabdillah312007 <133589931+azzamabdillah312007@users.noreply.github.com>
2023-10-04 14:38:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ body.innerHTML = `
         <button class = "menuSubmit"><i class="ri-menu-line"></i></button>
         <input style="input" type="text" placeholder="Enter the city names" required id="inputcity">
         <button class = "btnSubmit"><i class="ri-search-line"></i></button>
+        <button class = "btnreset" onclick="reset()"><i class="ri-refresh-line"></i></button>
     </div>
 </header>
 <div id="sidebar">
@@ -21,140 +22,145 @@ body.innerHTML = `
 <div class = 'isi semua'></div>
  
 `;  
+// bagian reset 
+const btnreset = document.querySelector(".btnreset")
+
+function reset() {
+  location.reload()
+}
 
 // bagian search
-const input = document.querySelector('#inputcity')
-const isi = document.querySelector('.isi')
+const isi = document.querySelector('.isi');
+const input = document.querySelector('#inputcity');
 const btnSearch = document.querySelector('.btnSubmit');
-btnSearch.onclick = () => {
-    fetch(`${wheatherApi}&q=${input.value}`)
-    .then(res => res.json())
-    .then(data => { 
-        console.log(data);
-        isi.innerHTML = `
-        <div class = 'date'>
-            <div class = 'city'>
-               <p>${data.forecast.forecastday[0].date}</p>
-               <h1>${data.location.name}</h1>
-               <h3>${data.current.condition.text}</h3>
-               <p><i class="ri-temp-cold-line"></i>${data.current.feelslike_c}<i class="ri-celsius-fill"></i></p>
+btnSearch.addEventListener('click' , ()=>{
+  fetch(`${wheatherApi}&q=${input.value}`)
+  .then(res => res.json())
+  .then(data => { 
+    console.log(data);
+    isi.innerHTML = `
+    <div class = 'date'>
+        <div class = 'city'> 
+           <p>${data.forecast.forecastday[0].date}</p>
+           <h1>${data.location.name}</h1>
+           <span>${data.location.country}</span>
+           <h3>${data.current.condition.text}</h3>
+           <p><i class="ri-temp-cold-line"></i>${data.current.feelslike_c}<i class="ri-celsius-fill"></i></p>
+        </div>
+    </div>
+    <div class = 'benner'>
+        <div class ="text">
+           <i class="ri-time-line"></i>
+           <p>phases of the moon and sun</p>
+        </div>
+        <div class ="fill">
+          <div class ="box moonrise">
+             <span>moonrise</span>
+             <i class="ri-moon-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].astro.moonrise}</p>
+          </div>
+
+          <div class ="box moonset">
+            <span>moonset</span>
+            <i class="ri-moon-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].astro.moonset}</p>
+          </div>
+
+          <div class ="box sunrise">
+            <span>sunrise</span>
+            <i class="ri-sun-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].astro.sunrise}</p>
+          </div>
+
+          <div class ="box sunset">
+            <span>sunset</span>
+            <i class="ri-sun-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].astro.sunset}</p>
+          </div>
+        </div>
+     </div>
+
+
+
+
+
+     
+     
+     <div class = 'benner'>
+        <div class ="text">
+           <i class="ri-time-line"></i>
+           <p>weather conditions at every hour</p>
+        </div>
+        <div class ="today">
+          <div class ="hour">
+             <span>00:00 AM</span>
+             <i class="ri-moon-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].hour[0].condition.text}</p>
+          </div>
+          <div class ="hour">
+             <span>06:00AM</span>
+             <i class="ri-sun-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].hour[6].condition.text}</p>
+          </div>
+          <div class ="hour">
+             <span>12:00 PM</span>
+             <i class="ri-sun-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].hour[12].condition.text}</p>
+          </div>
+          <div class ="hour">
+             <span>18:00 PM</span>
+             <i class="ri-moon-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].hour[18].condition.text}</p>
+          </div>
+          <div class ="hour">
+             <span>23:00 PM</span>
+             <i class="ri-moon-cloudy-fill"></i>
+            <p>${data.forecast.forecastday[0].hour[23].condition.text}</p>
+          </div>
+        </div>
+     </div>
+
+
+
+
+
+
+
+
+
+
+     <div class = 'benner'>
+        <div class ="nextDay">
+            <i class="ri-calendar-todo-line"></i>
+            <p>2-DAY FORECAST</p>
+        </div>
+        <div class ="afterTomorrow">
+            <div class ="hari harini">
+                <span>${data.forecast.forecastday[1].date}</span>
+                <div class = "maxtemp">
+                   <img src="https://${data.forecast.forecastday[1].day.condition.icon}" alt="">
+                </div>
+                <span>${data.forecast.forecastday[0].day.maxtemp_c}<i class="ri-celsius-fill"></i></span>
+            </div>
+
+            <div class ="hari besok">
+                <span>${data.forecast.forecastday[2].date}</span>
+                <div class = "maxtemp">
+                   <img src="https://${data.forecast.forecastday[2].day.condition.icon}" alt="">
+                </div>
+                <span>${data.forecast.forecastday[1].day.maxtemp_c}<i class="ri-celsius-fill"></i></span>
             </div>
         </div>
-        <div class = 'benner'>
-            <div class ="text">
-               <i class="ri-time-line"></i>
-               <p>phases of the moon and sun</p>
-            </div>
-            <div class ="fill">
-              <div class ="box moonrise">
-                 <span>moonrise</span>
-                 <i class="ri-moon-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].astro.moonrise}</p>
-              </div>
-
-              <div class ="box moonset">
-                <span>moonset</span>
-                <i class="ri-moon-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].astro.moonset}</p>
-              </div>
-
-              <div class ="box sunrise">
-                <span>sunrise</span>
-                <i class="ri-sun-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].astro.sunrise}</p>
-              </div>
-
-              <div class ="box sunset">
-                <span>sunset</span>
-                <i class="ri-sun-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].astro.sunset}</p>
-              </div>
-            </div>
-         </div>
+    </div>
 
 
+     <div class ="end">
+        <p>Last update : ${data.current.last_updated}</p>
+     </div>
+    `
+})
+})
 
-
-
-         
-         
-         <div class = 'benner'>
-            <div class ="text">
-               <i class="ri-time-line"></i>
-               <p>weather conditions at every hour</p>
-            </div>
-            <div class ="today">
-              <div class ="hour">
-                 <span>00:00 AM</span>
-                 <i class="ri-moon-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].hour[0].condition.text}</p>
-              </div>
-              <div class ="hour">
-                 <span>06:00AM</span>
-                 <i class="ri-sun-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].hour[6].condition.text}</p>
-              </div>
-              <div class ="hour">
-                 <span>12:00 PM</span>
-                 <i class="ri-sun-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].hour[12].condition.text}</p>
-              </div>
-              <div class ="hour">
-                 <span>18:00 PM</span>
-                 <i class="ri-moon-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].hour[18].condition.text}</p>
-              </div>
-              <div class ="hour">
-                 <span>23:00 PM</span>
-                 <i class="ri-moon-cloudy-fill"></i>
-                <p>${data.forecast.forecastday[0].hour[23].condition.text}</p>
-              </div>
-            </div>
-         </div>
-
-
-
-
-
-
-
-
-
-
-         <div class = 'benner'>
-            <div class ="nextDay">
-                <i class="ri-calendar-todo-line"></i>
-                <p>2-DAY FORECAST</p>
-            </div>
-            <div class ="afterTomorrow">
-                <div class ="hari harini">
-                    <span>Tomorrow</span>
-                    <div class = "maxtemp">
-                       <h2>${data.forecast.forecastday[0].day.maxtemp_c}<i class="ri-celsius-fill"></i></h2>
-                       <img src="https://${data.forecast.forecastday[1].day.condition.icon}" alt="">
-                    </div>
-                    <p>${data.forecast.forecastday[1].day.condition.text}</p>
-                </div>
-
-                <div class ="hari besok">
-                    <span>After Tomorrow</span>
-                    <div class = "maxtemp">
-                       <h2>${data.forecast.forecastday[1].day.maxtemp_c}<i class="ri-celsius-fill"></i></h2>
-                       <img src="https://${data.forecast.forecastday[2].day.condition.icon}" alt="">
-                    </div>
-                    <p>${data.forecast.forecastday[2].day.condition.text}</p>
-                </div>
-            </div>
-        </div>
-
-
-         <div class ="end">
-            <p>Last update : ${data.current.last_updated}</p>
-         </div>
-        `
-    }) 
-
-}
 
 // bagian sidebar masih bermasalah
 const btnSidebar = document.querySelector('.menuSubmit');
@@ -174,5 +180,3 @@ btnSidebar.addEventListener('click', () => {
     sideBar.style.left = "0";
   }
 });
-
-
